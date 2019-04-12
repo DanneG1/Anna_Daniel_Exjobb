7a33bff54a4e7ba222868c82d53c2bdc439b1fd0
annaforsb <anna-forsberg@live.com>
2019-04-12 14:30:51 +0200

    fixade så man kan  läsa in två filsökvägar

---
+++ ReadDllForm/ReadDllForm/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadDllForm
+{
+    public partial class Form1 : Form
+    {
+        private string hPath = "";
+        private string cppPath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openH = new OpenFileDialog();
+            openH.Filter="h files(*.h)|*.h";
+            if (openH.ShowDialog() == DialogResult.OK)
+            {
+                string hFileName = openH.FileName;
+                hPath = hFileName;
+                txtBoxH.Text = hFileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openCpp = new OpenFileDialog();
+            openCpp.Filter = "cpp files(*.cpp)|*.cpp";
+            if (openCpp.ShowDialog() == DialogResult.OK)
+            {
+                string cPpFileName = openCpp.FileName;
+                cppPath = cPpFileName;
+                txtBoxCpp.Text = cPpFileName;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (cppPath != "" && hPath != "")
+            {
+
+            }
+        }
+    }
+}
