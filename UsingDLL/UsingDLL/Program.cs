f9e6b7ed30645247a787cd991f5438fee0012ea5
DanneG1 <danne-g@hotmail.com>
2019-04-23 13:25:24 +0200

    la till så man kan skicka in directoryPath så man kan använda dll filen

--- UsingDLL/UsingDLL/Program.cs
+++ UsingDLL/UsingDLL/Program.cs
@@ -10,37 +10,56 @@ namespace UsingDLL
 {
     class Program
     {
-        //private const string Path = @"C:\Windows\SysWOW64\newDLLfileV2.dll";
+        private const string Path = @"C:\Windows\SysWOW64\newDLLfileV2.dll";
         //private const string Path = @"C:\Users\Danne\Documents\simulinkmodels\GenerateDLL.dll";
-        private const string Path = @"C:\Users\Danne\source\repos\Anna_Daniel_Exjobb\GenerateDLL\Debug\GenerateDLL.dll";
+        //private const string Path = @"C:\Users\Danne\source\repos\Anna_Daniel_Exjobb\GenerateDLL\Debug\GenerateDLL.dll";
         //private const string Path = @"C:\Users\Danne\source\repos\Anna_Daniel_Exjobb\GenerateDLL\x64\Debug\GenerateDLL.dll";
         //private const string Path = @"C:\Users\Danne\Documents\simulinkmodels\Annas\GenerateDLL.dll";
         //private const string Path = @"../../dll/GenerateDLL.dll";
+        private const string modelName = "newDLLfileV2.dll";
+        private static string directoryPath = @"C:\Windows\SysWOW64\";
+
+        public Program (string path)
+        {
+            directoryPath = path;
+        }
+
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetDllDirectory(string path);
+        [DllImport("kernel32", SetLastError = true)]
+        static extern IntPtr LoadLibrary(string path);
 
         [DllImport
-            (Path, CallingConvention = CallingConvention.Cdecl)]
+            (modelName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void initialize();
 
         [DllImport
-            (Path, CallingConvention = CallingConvention.Cdecl)]
+            (modelName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void step();
 
         [DllImport
-            (Path, CallingConvention = CallingConvention.Cdecl)]
+            (modelName, CallingConvention = CallingConvention.Cdecl)]
         public static extern double getOutputs(int port);
 
         [DllImport
-            (Path, CallingConvention = CallingConvention.Cdecl)]
+            (modelName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void setInputs(int port,double value);
 
         [DllImport
-            (Path, CallingConvention = CallingConvention.Cdecl)]
+            (modelName, CallingConvention = CallingConvention.Cdecl)]
         public static extern void terminate();
 
         static void Main(string[] args)
         {
             initialize();
             System.Console.WriteLine(Directory.GetCurrentDirectory());
+            SetDllDirectory(directoryPath);
+            LoadLibrary(directoryPath);
+
+
+
             setInputs(0, 1.0);
             setInputs(1, 1.0);
             setInputs(2, 1.0);
