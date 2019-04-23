2e496f66ff598f04bb2855b1050e1c74fdb2afc8
annaforsb <anna-forsberg@live.com>
2019-04-23 13:24:56 +0200

    lade till klass för modell

---
+++ ReadDllForm/ReadDllForm/SimulinkModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadDllForm
+{
+    class SimulinkModel
+    {
+        private const string ModelDll = @"ModelDll.dll";
+        private string DirectoryPath;
+
+        public SimulinkModel(string path)
+        {
+            DirectoryPath = path;
+        }
+
+        [DllImport
+            (ModelDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void initialize();
+
+        [DllImport
+            (ModelDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void step();
+
+        [DllImport
+            (ModelDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern double getOutputs(int port);
+
+        [DllImport
+            (ModelDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void setInputs(int port, double value);
+
+        [DllImport
+            (ModelDll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void terminate();
+
+        
+    }
+}
