18689c62540f3b01742e532da8092641e3c77c21
annaforsb <anna-forsberg@live.com>
2019-05-03 09:05:23 +0200

    tog bort onödiga referenser

--- ReadDllForm/ReadDllForm/Form1.cs
+++ ReadDllForm/ReadDllForm/Form1.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.Drawing.Text;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ReadDllForm.Properties;
-using HiQ.HiMacs.HiServices;
-using HiQ.HiMacs.WCF;
 using HiQ.HiMacs.WCF.Client;
 
 
